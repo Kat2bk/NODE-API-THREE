ba73b3633260ef0f3dfc1f70427d77ea1ed9030b
unknown <Katherine2bk@gmail.com>
2021-06-10 21:13:13 -0700

    logger middleware works

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,10 +1,23 @@
-function logger(req, res, next) {
-  // DO YOUR MAGIC
+function logger() {
+  return (req, res, next) => {
+  const time = new Date();
+  console.log(`${req.method} request to ${req.url} at ${time}`);
+  next();
+  }
 }
+
+// `logger()`
+//   - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+//   - this middleware runs on every request made to the API
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
 }
+
+// - `validateUserId()`
+//   - this middleware will be used for all user endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id.
+//   - if the `id` parameter is valid, store the user object as `req.user` and allow the request to continue
+//   - if the `id` parameter does not match any user id in the database, respond with status `404` and `{ message: "user not found" }`
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
@@ -22,17 +35,6 @@ module.exports = {
   validateUser,
   validatePost
 }
-
-// `logger()`
-
-//   - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
-//   - this middleware runs on every request made to the API
-
-// - `validateUserId()`
-
-//   - this middleware will be used for all user endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id.
-//   - if the `id` parameter is valid, store the user object as `req.user` and allow the request to continue
-//   - if the `id` parameter does not match any user id in the database, respond with status `404` and `{ message: "user not found" }`
 
 // - `validateUser()`
 
